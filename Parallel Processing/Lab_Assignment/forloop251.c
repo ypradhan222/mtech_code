62448edd64745b27ad2dd760c81ae0beba8e27d0
ypradhan222 <pradhanyogen63@gmail.com>
2023-08-01 20:48:58 +0200

    going on

--- Parallel Processing/Lab_Assignment/forloop251.c
+++ Parallel Processing/Lab_Assignment/forloop251.c
@@ -1,40 +1,34 @@
 #include<stdio.h>
-#include<omp.h>
-#include<stdlib.h>
-int main(){
-   int size;
-   printf("Enter the size of array::");
-   scanf("%d",&size);
-   if (size>=0) {
-   int *array1 = (int *)malloc(sizeof(int) *size);
-   int *array2 = (int *)malloc(sizeof(int) *size);
-   int *array3 = (int *)malloc(sizeof(int) *size);
+#include <omp.h>
+#define N     1000
 
-   for (int i = 0; i < size; i++)
-   {
-     array1[i] = rand()%100+1;  /* code */
-   }
-   for (int i = 0; i < size; i++)
-   {
-    array2[i] = rand()%100+1;  /* code */
-   }
-   omp_set_num_threads(4);
-    #pragma omp parallel for
-    for (int i = 0; i < size; i++)
+int main ()
+{
+
+int i;
+float a[N], b[N], c[N], d[N];
+
+for (i=0; i < N; i++) {
+  a[i] = i * 1.5;
+  b[i] = i + 22.35;
+  }
+
+#pragma omp parallel shared(a,b,c,d) private(i)
+  {
+
+  #pragma omp sections nowait
     {
-     array3[i]  = array1[i]+array2[i];/* code */
-    }
-   //PRinting the resultant array
-   for (int i = 0; i < size; i++)
-   {
-    printf("%d",array3[i]);  /* code */
-   }
-   
-   }
-   else{
-      printf("ERROR Size cannot be negative.");
-   }
-   
-   return 0;
 
+    #pragma omp section
+    for (i=0; i < N; i++)
+      c[i] = a[i] + b[i];
+
+    #pragma omp section
+    for (i=0; i < N; i++)
+      d[i] = a[i] * b[i];
+
+    }  
+
+  }  /* end of parallel section */
+return 0;
 }
--- Parallel Processing/Lab_Assignment/sections.c
+++ Parallel Processing/Lab_Assignment/sections.c
@@ -1,56 +1,43 @@
-#define SIZE 1000000
 #include<stdio.h>
-#include<omp.h>
-#include<stdlib.h>
-int gammaa(int a,int b);
-int alpha();
-int beta();
-int delta();
-float epsilon(int u,int v);
-int alpha(){
-   int x=1;
-   for(int i ;i<SIZE;i++)
-   x = x+i;
-   return x;
-}
+#include <omp.h>
+#define N     1000
 
-int beta(){
-   int y=1;
-   for (int i = 0; i < SIZE; i++)
-   {
-     y = (y*i)+2; /* code */
-   }
-   return y;
-}
-int gammaa(int a,int b){
-   return a*b;
-}
-int delta(){
-   int z=2;
-   for (int i = 0; i < SIZE; i++)
-   {
-     z = 2*z*i; /* code */
-   }
-   return z;
-}
-float epsilon(int u,int v){
-   int h ;
-   h = (u*v)/2;
-   return h;
-}
-int main(){
-   int x,y,z;
-   int v;
-   #pragma omp sections private(x,y,z,v)
-   {
-      #pragma omp section
-         x = alpha();
-      #pragma omp section
-         y = beta(); 
-      #pragma omp section
-         z - delta();
-   }
-   v = gammaa(x,y);
-   printf("Epsilon:: %6.2f",epsilon(v,z));
-   return 0;
+int main ()
+{
+
+int i;
+float a[N], b[N], c[N], d[N];
+
+for (i=0; i < N; i++) {
+  a[i] = i * 1.5;
+  b[i] = i + 22.35;
+  }
+
+#pragma omp parallel 
+  {
+
+  #pragma omp sections 
+    {
+
+    #pragma omp section
+    for (i=0; i < N; i++)
+      c[i] = a[i] + b[i];
+
+    #pragma omp section
+    for (i=0; i < N; i++)
+      d[i] = a[i] * b[i];
+
+    }  
+
+  }  
+  for (int i = 0; i < N; i++)
+  {
+   printf("%f\n",c[i]);/* code */
+  }
+  for (int i = 0; i < N; i++)
+  {
+   printf("%f",d[i]);/* code */
+  }
+  
+return 0;
 }
