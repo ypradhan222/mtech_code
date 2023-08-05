fd44d39dc6decbe8dd8a03165565fc14c59e80fc
ypradhan222 <pradhanyogen63@gmail.com>
2023-08-05 13:48:57 +0200

    assignmnt going on

--- Parallel Processing/matrix_multi.c
+++ Parallel Processing/matrix_multi.c
@@ -1,66 +1,86 @@
 #include<stdio.h>
 #include<time.h>
 #include<stdlib.h>
-int main(){
-int rows,columns;
-int rows2,columns2;
-int result[rows][columns];
-int array1[rows2][columns2];
-int array2[1000][1000];
-printf("Enter the number of rows::");
-scanf("%d %d",&rows,&columns);
-printf("Enter the number of columns::");
-scanf("%d %d",&rows2,&columns2);
-//adding elements to the arrays
-for (int i = 0; i < rows; i++)
+
+void addElements(int **array1,int size){
+for (int i = 0; i < size; i++)
 {
-for (int j = 0; j < columns; j++)
+for (int j = 0; j < size; j++)
 {
-    array1[i][j] = rand()%100+1;
+  array1[i][j] = rand()%100+1;
 }
-}
-for (int i = 0; i < rows; i++)
-{
-for (int j = 0; j < columns; j++)
-{
-  array2[i][j]= rand()%100+1;
-}
-}
-for (int i = 0; i < rows; i++)
-{
-for (int j = 0; j < columns; j++)
-{
-  printf("%d\t",array1[i][j]);
 }
 }
 
-for (int i = 0; i < rows; i++)
+void printMatrix(int **array1,int size){
+for (int i = 0; i < size; i++)
 {
-for (int j = 0; j < columns; j++)
+for (int j = 0; j < size; j++)
 {
-    printf("%d\t",array2[i][j]);
-}
-}
-//Multiplying the matrices
-for (int i = 0; i < rows; i++)
-{
- for (int j = 0; j < columns2; j++)
- {
-  result[i][j]=0;
-  for (int k = 0; k < columns2; k++)
-  {
-    result[i][j] =  array1[i][k] * array2[k][j];
-  }
- }
-}
-
-for (int i = 0; i < rows; i++)
-{
-for (int j = 0; j < columns; j++)
-{
-  printf("%d\t",result[i][j]);  /* code */
+  printf("%8d",array1[i][j]);
 }
 printf("\n");
 }
+}
+int main(){
+int size;
+printf("Enter the size::");
+scanf("%d",&size);
+srand(time(NULL));
+int **array1 = (int **)malloc(sizeof(int *) *size);
+for (int i = 0; i < size; i++)
+{
+ array1[i] = (int *)malloc(sizeof(int) *size); /* code */
+}
+
+int **array2 = (int **)malloc(sizeof(int*) *size);
+for (int i = 0; i < size; i++)
+{
+ array2[i] = (int *)malloc(sizeof(int) *size); /* code */
+}
+
+int **result = (int **)malloc(sizeof(int*) *size);
+for (int i = 0; i < size; i++)
+{
+ result[i] = (int *)malloc(sizeof(int) *size); /* code */
+}
+//adding elements to the arrays
+  addElements(array1,size);
+  addElements(array2,size);
+  printf("Array 1 :: \n");
+  printMatrix(array1,size);
+  printf("\n");
+  printf("Array 2 :: \n");
+  printMatrix(array2,size);
+//Multiplying the matrices
+
+for (int i = 0; i < size; i++)
+{
+ for (int j = 0; j < size; j++)
+ {
+  result[i][j]=0;
+  for (int k = 0; k < size; k++)
+  {
+    result[i][j] +=  array1[i][k] * array2[k][j];
+  }
+ }
+}
+printf("Resultant matrix::\n");
+printMatrix(result,size);
+
+for (int i = 0; i < size; i++)
+{
+ free(array1[i]);
+ free(array2[i]); /* code */
+}
+free(array1);
+free(array2);
+for (int i = 0; i < size; i++)
+{
+ free(result[i]); /* code */
+}
+
+free(result);
+
 return 0;
 }
