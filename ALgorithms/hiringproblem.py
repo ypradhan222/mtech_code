f469fbfec7b8f24aa57d7fb5f1684effc00c2896
ypradhan222 <pradhanyogen63@gmail.com>
2023-09-01 10:01:43 +0200

    hiring added

--- ALgorithms/hiringproblem.py
+++ ALgorithms/hiringproblem.py
@@ -1,11 +1,24 @@
-def hiring(array,threshold):
-   current_candidate = 0 
-   # array=[]
-   while i<n:
-      if i<threshold:
-         if array[i]>current_candidate:
-            current_candidate = array[i]
-      else:
-         if array[i]>current_candidate:
-            return array[i]
-      i+=1
+import random
+def hiring(array, threshold):
+  current_candidate = 0
+  i = 0
+  while i < len(array):
+    if array[i] > current_candidate:
+      current_candidate = array[i]
+    elif i >= threshold and i < len(array):
+      return array[i]
+    i += 1
+  return current_candidate
+
+def randomized(array):
+   priority=[]
+   for i in range(len(array)):
+      priority.append(random.randint(1,len(array)**3))
+      shuffled_array = [x for _,x in sorted(zip(priority,array))]
+   return shuffled_array
+
+array = [random.randint(1,10) for _ in range(100)]
+random_list = randomized(array)
+threshold = 20
+hired_candidate = hiring(random_list,threshold)
+print("The hired candidate:: ",hired_candidate)  
